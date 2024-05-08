c36988886b75e997504e2a920833404105587b8f
JackRen88 <2541123296@qq.com>
2024-05-08 22:42:16 +0800

    modify crying_baby_problem.py

--- pomdp_problems/crying_baby/crying_baby_problem.py
+++ pomdp_problems/crying_baby/crying_baby_problem.py
@@ -36,21 +36,26 @@ import random
 import numpy as np
 import sys
 
+
 class CryingBabyState(pomdp_py.State):
     def __init__(self, name):
         self.name = name
-        assert self.name in {"hungry","full"}, "state is invalid,%s" % self.name
-            
-    #dic request
+        assert self.name in {
+            "hungry", "full"}, "state is invalid,%s" % self.name
+
+    # dic request
     def __hash__(self):
         return hash(self.name)
-    #dic request
+    # dic request
+
     def __eq__(self, other):
         if isinstance(other, CryingBabyState):
             return self.name == other.name
         return False
+
     def __str__(self):
         return self.name
+
     def __repr__(self):
         return "CryingBabyState(%s)" % self.name
     # def other(self):
@@ -59,39 +64,53 @@ class CryingBabyState(pomdp_py.State):
     #     else:
     #         return CryingBabyState("hungry")
 
+
 class CryingBabyAction(pomdp_py.Action):
     def __init__(self, name):
         self.name = name
-        assert self.name in {"feed","ignore"}, "action is invalid,%s" % self.name
+        assert self.name in {
+            "feed", "ignore"}, "action is invalid,%s" % self.name
+
     def __hash__(self):
         return hash(self.name)
+
     def __eq__(self, other):
         if isinstance(other, CryingBabyAction):
             return self.name == other.name
         return False
+
     def __str__(self):
         return self.name
+
     def __repr__(self):
         return "CryingBabyAction(%s)" % self.name
+
 
 class CryingBabyObservation(pomdp_py.Observation):
     def __init__(self, name):
         self.name = name
-        assert self.name in {"crying","quiet"}, "Observation is invalid,%s" % self.name
+        assert self.name in {
+            "crying", "quiet"}, "Observation is invalid,%s" % self.name
+
     def __hash__(self):
         return hash(self.name)
+
     def __eq__(self, other):
         if isinstance(other, CryingBabyObservation):
             return self.name == other.name
         return False
+
     def __str__(self):
         return self.name
+
     def __repr__(self):
         return "CryingBabyObservation(%s)" % self.name
 
 # Observation model
+
+
 class ObservationModel(pomdp_py.ObservationModel):
-    def __init__(self, noise_when_hungry = 0.2,noise_when_full = 0.1):
+    def __init__(self, noise_when_hungry=0.2, noise_when_full=0.1):
         self.noise_when_hungry = noise_when_hungry
         self.noise_when_full = noise_when_full
 
@@ -110,17 +129,17 @@ class ObservationModel(pomdp_py.ObservationModel):
     def sample(self, next_state, action):
         if next_state.name == "hungry":
             thresh = self.noise_when_hungry
-            if random.uniform(0,1) < thresh:
+            if random.uniform(0, 1) < thresh:
                 return CryingBabyObservation("quiet")
             else:
                 return CryingBabyObservation("crying")
         else:
             thresh = self.noise_when_full
-            if random.uniform(0,1) < thresh:
+            if random.uniform(0, 1) < thresh:
                 return CryingBabyObservation("crying")
             else:
                 return CryingBabyObservation("quiet")
-            
+
     def get_all_observations(self):
         """Only need to implement this if you're using
         a solver that needs to enumerate over the observation space
@@ -129,62 +148,68 @@ class ObservationModel(pomdp_py.ObservationModel):
                 for s in {"crying", "quiet"}]
 
 # Transition Model
+
+
 class TransitionModel(pomdp_py.TransitionModel):
     def probability(self, next_state, state, action):
         if state.name.startswith("hungry") and action.name.startswith("feed"):
             if next_state.name.startswith("hungry"):
-              return 0.0
+                return 0.0
             else:
-              return 1.0
+                return 1.0
         if state.name.startswith("hungry") and action.name.startswith("ignore"):
             if next_state.name.startswith("hungry"):
-              return 1.0
+                return 1.0
             else:
-              return 0.0
+                return 0.0
         if state.name.startswith("full") and action.name.startswith("feed"):
             if next_state.name.startswith("hungry"):
-              return 0.0
+                return 0.0
             else:
-              return 1.0
+                return 1.0
         if state.name.startswith("full") and action.name.startswith("ignore"):
             if next_state.name.startswith("hungry"):
-              return 0.1
+                return 0.1
             else:
-              return 0.9          
+                return 0.9
 
     def sample(self, state, action):
         if action.name.startswith("feed"):
             return CryingBabyState("full")
         else:
             if state.name.startswith("hungry"):
-              return CryingBabyState("hungry")
+                return CryingBabyState("hungry")
             else:
                 thresh = 0.1
-                if random.uniform(0,1) < thresh:
+                if random.uniform(0, 1) < thresh:
                     return CryingBabyState("hungry")
                 else:
                     return CryingBabyState("full")
-                
+
     def get_all_states(self):
         """Only need to implement this if you're using
         a solver that needs to enumerate over the observation space (e.g. value iteration)"""
         return [CryingBabyState(s) for s in {"hungry", "full"}]
 
 # Reward Model
+
+
 class RewardModel(pomdp_py.RewardModel):
     def _reward_func(self, state, action):
-      return (-10 if state.name.startswith("hungry") else 0) + (-8 if action.name.startswith("feed") else 0)
+        return (-10 if state.name.startswith("hungry") else 0) + (-8 if action.name.startswith("feed") else 0)
 
     def sample(self, state, action, next_state):
         # deterministic
         return self._reward_func(state, action)
 
 # Policy Model
+
+
 class PolicyModel(pomdp_py.RolloutPolicy):
     """A simple policy model with uniform prior over a
        small, finite action space"""
     ACTIONS = [CryingBabyAction(s)
-              for s in {"feed", "ignore"}]
+               for s in {"feed", "ignore"}]
 
     def sample(self, state):
         return random.sample(self.get_all_actions(), 1)[0]
@@ -209,7 +234,8 @@ class CryingBabyProblem(pomdp_py.POMDP):
         agent = pomdp_py.Agent(init_belief,
                                PolicyModel(),
                                TransitionModel(),
-                               ObservationModel(noise_when_hungry,noise_when_full),
+                               ObservationModel(
+                                   noise_when_hungry, noise_when_full),
                                RewardModel())
         env = pomdp_py.Environment(init_true_state,
                                    TransitionModel(),
@@ -217,7 +243,7 @@ class CryingBabyProblem(pomdp_py.POMDP):
         super().__init__(agent, env, name="CryingBabyProblem")
 
     @staticmethod
-    def create(state="full", belief=0.5, noise_when_hungry=0.2,noise_when_full=0.1):
+    def create(state="full", belief=0.5, noise_when_hungry=0.2, noise_when_full=0.1):
         """
         Args:
             state (str): could be 'hungry' or 'full';
@@ -232,12 +258,10 @@ class CryingBabyProblem(pomdp_py.POMDP):
             CryingBabyState("hungry"): belief,
             CryingBabyState("full"): 1.0 - belief
         })
-        crying_baby_problem = CryingBabyProblem(noise_when_hungry,noise_when_full,
-                                     init_true_state, init_belief)
+        crying_baby_problem = CryingBabyProblem(noise_when_hungry, noise_when_full,
+                                                init_true_state, init_belief)
         crying_baby_problem.agent.set_belief(init_belief, prior=True)
         return crying_baby_problem
-
-
 
 
 def test_planner(crying_baby_problem, planner, nsteps=3,
@@ -257,7 +281,8 @@ def test_planner(crying_baby_problem, planner, nsteps=3,
         if debug_tree:
             # from pomdp_py.utils import TreeDebugger
             dd = TreeDebugger(crying_baby_problem.agent.tree)
-            import pdb; pdb.set_trace()
+            import pdb
+            pdb.set_trace()
 
         if crying_baby_problem.env.state.name.startswith("full") and action.name.startswith("feed"):
             """
@@ -283,7 +308,6 @@ def test_planner(crying_baby_problem, planner, nsteps=3,
         reward = crying_baby_problem.env.state_transition(action, execute=True)
         print("Reward:", reward)
         print("True next env state:", crying_baby_problem.env.state)
-        
 
         # Let's create some simulated real observation;
         # Here, we use observation based on true state for sanity
@@ -295,13 +319,14 @@ def test_planner(crying_baby_problem, planner, nsteps=3,
         # or coming from an external source (e.g. robot sensor
         # reading). Note that here crying_baby_problem.env.state stores the
         # environment state after action execution
-        real_observation = crying_baby_problem.agent.observation_model.sample(crying_baby_problem.env.state, action)
+        real_observation = crying_baby_problem.agent.observation_model.sample(
+            crying_baby_problem.env.state, action)
         print(">> Observation:",  real_observation)
         crying_baby_problem.agent.update_history(action, real_observation)
 
         # Update the belief. If the planner is POMCP, planner.update
         # also automatically updates agent belief.
-        #TODO(all):here select method of update belief according to planner 
+        # TODO(all):here select method of update belief according to planner
         planner.update(crying_baby_problem.agent, action, real_observation)
         if isinstance(planner, pomdp_py.POUCT):
             print("Num sims:", planner.last_num_sims)
@@ -317,15 +342,14 @@ def test_planner(crying_baby_problem, planner, nsteps=3,
             crying_baby_problem.agent.set_belief(new_belief)
             # print("new_belief: ",new_belief)
 
-        
 
 def main():
     init_true_state = random.choice([CryingBabyState("hungry"),
                                      CryingBabyState("full")])
     init_belief = pomdp_py.Histogram({CryingBabyState("hungry"): 0.5,
                                       CryingBabyState("full"): 0.5})
-    crying_baby_problem = CryingBabyProblem(0.2, 0.1, # when full or hungry observation noise
-                                 init_true_state, init_belief)
+    crying_baby_problem = CryingBabyProblem(0.2, 0.1,  # when full or hungry observation noise
+                                            init_true_state, init_belief)
 
     # print("** Testing value iteration **")
     """     
@@ -368,5 +392,7 @@ def main():
     # test_planner(crying_baby_problem, pomcp, nsteps=10)
     # TreeDebugger(tiger_problem.agent.tree).pp
 
+
 if __name__ == '__main__':
     main()
+#print hello world
